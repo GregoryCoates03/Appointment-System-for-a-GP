81d424c7a95d4735cfeeb6759a0532a114bdbc75
Gregory Coates <78648883+GregoryCoates03@users.noreply.github.com>
2024-03-22 19:20:41 +0000

    Updated dbSetup.sql.

--- src/dbSetup.sql
+++ src/dbSetup.sql
@@ -1,8 +1,13 @@
+CREATE TABLE IF NOT EXISTS locations (
+    location_id SERIAL PRIMARY KEY,
+    location_name VARCHAR(50)
+);
+
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50),
     family_name VARCHAR(50),
-    email VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
     phone_number VARCHAR(20),
     address VARCHAR(50),
     password VARCHAR(50),
@@ -10,16 +15,27 @@ CREATE TABLE IF NOT EXISTS users (
     waiting_list_position INT DEFAULT 0
 );
 
-SELECT * FROM users;
-
 CREATE TABLE IF NOT EXISTS appointments (
     appointment_id SERIAL PRIMARY KEY,
     user_id INT,
+    location_id INT,
+    doctor_id INT,
     appointment_type VARCHAR(50),
     practitioner VARCHAR(50),
     time TIME,
     date DATE,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) references users(user_id)
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+    CONSTRAINT fk_location_id FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
 );
 
-SELECT * FROM appointments;
+CREATE TABLE IF NOT EXISTS doctors (
+    doctor_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    location_id INT,
+    start_time TIME,
+    end_time TIME,
+    break_time TIME,
+    CONSTRAINT fk_location_id FOREIGN KEY (location_id) REFERENCES locations(location_id)
+);
