708a2148114bf2ab3d946e46f1cbf1bccc310bd7
GregoryCoates03 <78648883+GregoryCoates03@users.noreply.github.com>
2024-03-26 19:53:45 +0000

    Added admin functionality to add risk labels.

--- src/webPages/UpdateRisk.js
+++ src/webPages/UpdateRisk.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const UpdateRisk = (props) => {
@@ -7,13 +7,38 @@ const UpdateRisk = (props) => {
     const [email, setEmail] = useState("");
     const [risk, setRisk] = useState(false);
 
-    const handleSubmit = async () => {
-
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.put(`http://localhost:3001/api/risk`, { risk, email });
+            console.log(response.data);
+            const error = document.getElementById('error');
+            if (response.data && (response.data[0].at_risk === risk)) {
+                if (risk === true){
+                    error.textContent = `Risk label added to user: ${email}`;
+                } else {
+                    error.textContent = `Risk label removed from user: ${email}`;
+                }
+                error.className = "text-green-500";
+            } else {
+                error.textContent = "ERROR";
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     const handleChange = (event) => {
-        
+        setEmail(event.target.value);
     }
+
+    const handleRisk = () => {
+        setRisk(!risk);
+    }
+
+    useEffect(() => {
+        console.log(risk);
+    }, [risk])
 
     if (admin) {
         return (
@@ -22,6 +47,8 @@ const UpdateRisk = (props) => {
                 <form className="flex flex-col items-center" onSubmit={handleSubmit}>
                     <label htmlFor="email">User Email:</label>
                     <input id="email" name="email" type="text" className="bg-gray-400" value={email} onChange={handleChange}/>
+                    <label htmlFor="risk">Risk:</label>
+                    <input id="risk" name="risk" type="checkbox" value={risk} onChange={handleRisk}/>
                     <button type="submit" className="text-lime-500">Update Risk</button>
                 </form>
                 <p id="error" className="text-red-500"></p>
